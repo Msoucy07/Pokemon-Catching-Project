0a20281252953cd359f8838851d3c2e926fe6125
Msoucy07 <matthew.soucy1@maine.edu>
2025-04-25 13:02:26 -0400

    Update main.c

--- main.c
+++ main.c
@@ -20,33 +20,4 @@ int main(){
     int count = 0;
     while (1) {
         printf("1. Catch Pokemon\n2. Print team\n3. Save team\n4. Load team\n5. Exit\n");
-        scanf("%d", &choice);
-        getchar();
-
-        switch (choice) {
-            case 1:
-                if (count < TEAM_SIZE) {
-                    team[count++] = spawn_random_pokemon();
-                } else {
-                    printf("team is full!\n");
-                }
-                break;
-            case 2:
-                print_team(team, count);
-                break;
-            case 3:
-                // save stuff
-                break;
-            case 4:
-                // load stuff
-                break;
-            case 5:
-                free(team);
-                printf("Goodbye!\n");
-                return 0;
-            default:
-                printf("Invalid option.\n");
-
-        }
     }
-}
